92323f95827ace5a316c2e128a5582654db61108
joaopapapa2025-ux <joao.papapa2025@gmail.com>
2026-04-29 13:34:53 -0300

    Update playbook.py

--- playbook.py
+++ playbook.py
@@ -386,6 +386,7 @@ elif aba_selecionada == "📄 Biblioteca de Arquivos":
             "🏛️ Inscrição Municipal": "INSCRIÇÃO MUNICIPAL.pdf",
             "📑 Sintegra": "SINTEGRA PAPAPÁ.pdf",
             "💰 Comprovante Bancário": "COMPROVANTE BANCÁRIO (1).png"
+            "🏥 Licença sanitária": "COMPROVANTE BANCÁRIO (1).png"
         }
         for label, path in arquivos_fiscais.items():
             try:
