d2ae2f877eb72f9ea56b3d90d5e7a8c44c4d03d1
joaopapapa2025-ux <joao.papapa2025@gmail.com>
2026-05-15 15:28:43 -0300

    Update playbook.py

--- playbook.py
+++ playbook.py
@@ -138,7 +138,7 @@ st.markdown(f"""
     <div class="header-container">
         <img src="{img_logo_html}" width="300">
         <h1 style='color: #004a99; font-family: sans-serif; font-weight: 850; margin-top: 10px;'>
-            Hub Inside Sales
+            Hub - Comercial
         </h1>
     </div>
     """, unsafe_allow_html=True)
