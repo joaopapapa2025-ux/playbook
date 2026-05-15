4268e8f0749cd3697aaeae7c955ca65da06c3053
joaopapapa2025-ux <joao.papapa2025@gmail.com>
2026-05-15 09:01:36 -0300

    Update playbook.py

--- playbook.py
+++ playbook.py
@@ -1910,7 +1910,7 @@ elif aba_selecionada == "🛒 Simulador de Pedidos":
                         # Aviso Legal
                         pdf.ln(10)
                         pdf.set_font("Arial", "I", 8)
-                        aviso = u"*Simulação de valores. Sem validade fiscal ou reserva de estoque."
+                        aviso = u"*Este documento é apenas uma simulação de valores (orçamento) e não garante a reserva de estoque ou a efetivação do pedido comercial. Este informativo não possui validade fiscal."
                         pdf.multi_cell(190, 5, txt=aviso.encode('latin-1', 'ignore').decode('latin-1'), align='C')
                         
                         return pdf.output(dest='S').encode('latin-1')
