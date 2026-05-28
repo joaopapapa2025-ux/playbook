5d0d9e21a2c328e77541e9eae81ca940ea4efc48
joaopapapa2025-ux <joao.papapa2025@gmail.com>
2026-05-28 16:28:16 -0300

    Update playbook.py

--- playbook.py
+++ playbook.py
@@ -2377,7 +2377,7 @@ elif aba_selecionada == "🛒 Simulador de Pedidos":
                 observacoes_pedido = st.text_area(
                     "Observações",
                     placeholder="Inclua informações relevantes sobre o orçamento...",
-                    height=380,
+                    height=378,
                     key="sim_observacoes"
                 )
 
